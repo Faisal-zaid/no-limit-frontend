a76c860d5e16bd7646cbc431616691469bd26408
faisal ali zaid <zaidalideveloper@gmail.com>
2026-07-21 14:56:59 +0300

    inital codes for hero

---
+++ src/app/components/hero.js
@@ -0,0 +1,10 @@
+export default function Hero() {
+    return (
+        <section>
+            <h1>Welcome to No Limits</h1>
+            <p>Premium products tailored to your needs.</p>
+
+            <button>Shop Now</button>
+        </section>
+    );
+}
