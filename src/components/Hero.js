72667e136f1f5ffb80703bb7868ac46251b79479
faisal ali zaid <zaidalideveloper@gmail.com>
2026-07-21 16:00:43 +0300

    changed how navbar is positioned

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,17 +1,29 @@
+import Image from "next/image"; //this line imports image itself
+
+
 export default function Hero() {
     return (
         <header>
             <div class="upper-nav">
                 <ul>
+                     <li>
+                         <Image
+                src="/images/no-limit-logo.png"
+                alt="Hero Image"
+                width={700}
+                height={500}
+            />
+                    </li>
                     <li>NAIROBI,Ke</li>
-                    <li>CUSTOM BRANDING & MERCHANDISE</li>
                     <li></li>
-                    <li>NO LIMIT BRANDS</li>
+                    <li>CUSTOM BRANDING & MERCHANDISE</li>
+                    
+                   
                 </ul>
             </div>
             <div className="lower-nav">
                 <ul>
-                    <li></li>
+                     <li>NO LIMIT BRANDS</li>
                     <li></li>
                     <li></li>
                 </ul>
