c66f6255cd0d74048de774e0d8a7682d5934855e
faisal ali zaid <zaidalideveloper@gmail.com>
2026-07-22 15:12:11 +0300

    switched from class to className

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -56,8 +56,8 @@ export default function Hero() {
           </div>
           {/* div for buttons */}
           <div className="flex gap-[1%] ml-[5%]">
-                <button class="bg-black text-white border pr-[1%] pl-[1%] rounded-[10] ">VISIT SHOP</button>
-                <button class="border border-2 border-black-500 pr-[1%] pl-[1%] rounded-[10]">CONTACT US</button>
+                <button className="bg-black text-white border pr-[1%] pl-[1%] rounded-[10] ">VISIT SHOP</button>
+                <button className="border border-2 border-black-500 pr-[1%] pl-[1%] rounded-[10]">CONTACT US</button>
             </div>
         </div>
         <div className="w-1/2">
