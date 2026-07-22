74b3b7da67a249d43941998fdd470888a2731fcd
faisal ali zaid <zaidalideveloper@gmail.com>
2026-07-22 15:10:10 +0300

    changed from contact me to contact us

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -57,7 +57,7 @@ export default function Hero() {
           {/* div for buttons */}
           <div className="flex gap-[1%] ml-[5%]">
                 <button class="bg-black text-white border pr-[1%] pl-[1%] rounded-[10] ">VIEW PROJECTS</button>
-                <button class="border border-2 border-black-500 pr-[1%] pl-[1%] rounded-[10]">CONTACT ME</button>
+                <button class="border border-2 border-black-500 pr-[1%] pl-[1%] rounded-[10]">CONTACT US</button>
             </div>
         </div>
         <div className="w-1/2">
