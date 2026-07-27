7191e7347a7b4ac0bcedae68d530132bcb012d27
faisal ali zaid <zaidalideveloper@gmail.com>
2026-07-27 14:31:56 +0300

    declaration of the function

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,3 +1,9 @@
 'use client';  //this tells next js this component should run on the client not only in the server side
 
-import { useSearchParams, usePathname, useRouter } from 'next/navigation'; // pathname tells you the current path, router lets you navigate to another route and params reads quesry parameters
+import { useSearchParams, usePathname, useRouter } from 'next/navigation'; // pathname tells you the current path, router lets you navigate to another route and params reads quesry 
+
+export default function SearchBar() {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+}
