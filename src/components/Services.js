75986c767727ca942db8513160e28d5635fe27e7
faisal ali zaid <zaidalideveloper@gmail.com>
2026-07-27 13:10:25 +0300

    moved services title a little

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,7 +1,9 @@
 export default function Services() {
   return (
     <section>
-      <h2>Our Services</h2>
+      <div className="text-[20px] ml-[3%]">
+        <h2 >Our Services</h2>
+      </div>
     </section>
   );
 }
