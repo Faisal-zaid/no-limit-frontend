ab370dcb649f9755cc1959a130cbbad13014eae6
faisal ali zaid <zaidalideveloper@gmail.com>
2026-07-27 14:33:34 +0300

    added comment for search param

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,7 +3,7 @@
 import { useSearchParams, usePathname, useRouter } from 'next/navigation'; // pathname tells you the current path, router lets you navigate to another route and params reads quesry 
 
 export default function SearchBar() {
-  const searchParams = useSearchParams();
+  const searchParams = useSearchParams(); // returns exactly what is needed from the url
   const pathname = usePathname();
   const { replace } = useRouter();
 }
