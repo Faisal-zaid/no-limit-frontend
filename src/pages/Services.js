736c309945479e1a303ce8e830b05f9c90d3cd61
faisal ali zaid <zaidalideveloper@gmail.com>
2026-07-27 15:23:24 +0300

    formatted doc

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,19 +1,15 @@
-import Search from '@/components/Search';
+import Search from "@/components/Search";
 
 export default function Services() {
   return (
     <section>
       <div className="text-[20px] ml-[3%] mt-[5%]">
-        <Search/>
+        <Search />
       </div>
 
-      <div className='content-area'>
-<div className='categories'>
-
-</div>
-<div className='category info'>
-
-</div>
+      <div className="content-area">
+        <div className="categories"></div>
+        <div className="category info"></div>
       </div>
     </section>
   );
