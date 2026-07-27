fd3785f860785f03eb4e8a425bbbd39d0329e0c9
faisal ali zaid <zaidalideveloper@gmail.com>
2026-07-27 14:54:30 +0300

    added the search field to services page

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,8 +1,10 @@
+import Search from '@/components/Search';
+
 export default function Services() {
   return (
     <section>
-      <div className="text-[20px] ml-[3%]">
-        <h2 >Our Services</h2>
+      <div className="text-[20px] ml-[3%] mt-[5%]">
+        <Search/>
       </div>
     </section>
   );
