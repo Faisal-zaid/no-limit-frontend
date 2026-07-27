59ab914451619ac6255a682952dba3f18214d3e9
faisal ali zaid <zaidalideveloper@gmail.com>
2026-07-27 14:42:20 +0300

    added code for search functionality

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,4 +6,15 @@ export default function SearchBar() {
   const searchParams = useSearchParams(); // returns exactly what is needed from the url
   const pathname = usePathname(); // gets the current path you are on. e.g maybe you are on products it will let you know that
   const { replace } = useRouter(); // suppose initial link was '/products' now after replace it can be '/products?search=laptop'
+  
+  function handleSearch(term) {
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set('q', term);
+    } else {
+      params.delete('q');
+    }
+    // Updates the URL without reloading the page (e.g., /shop?q=shirts)
+    replace(`${pathname}?${params.toString()}`);
+  }
 }
