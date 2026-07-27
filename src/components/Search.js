a2b8f6fe4c2a022994787503c70bffd5e0ce6945
faisal ali zaid <zaidalideveloper@gmail.com>
2026-07-27 14:30:19 +0300

    imported

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,1 +1,3 @@
 'use client';  //this tells next js this component should run on the client not only in the server side
+
+import { useSearchParams, usePathname, useRouter } from 'next/navigation'; // pathname tells you the current path, router lets you navigate to another route and params reads quesry parameters
