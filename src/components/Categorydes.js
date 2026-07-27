a12cf8457f67bc57232797f20edf6219451c4d8b
faisal ali zaid <zaidalideveloper@gmail.com>
2026-07-27 15:51:46 +0300

    added code for fetching category description from backend

--- src/components/Categorydes.js
+++ src/components/Categorydes.js
@@ -1,1 +1,32 @@
 // here it will hold description for categories
+
+'use client';  //this tells next js this component should run on the client not only in the server side
+
+import { useEffect, useState } from "react";
+
+export default function Categorydescription() {
+  const [categories, setCategories] = useState([]);
+
+   useEffect(() => {
+    async function loadCategories() {
+      const response = await fetch("http://127.0.0.1:8001/category");
+      const data = await response.json();
+
+      setCategories(data);
+    }
+
+    loadCategories();
+  }, []);
+
+  return (
+    <div>
+      <h2>Categories</h2>
+
+      {categories.map((category) => (
+        <div key={category.id}>
+          <h3>{category.name}</h3>
+        </div>
+      ))}
+    </div>
+  );
+}
