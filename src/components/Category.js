dcc6d82235e361f113c308566c2aaa095999dc55
faisal ali zaid <zaidalideveloper@gmail.com>
2026-07-28 14:57:54 +0300

    made a fix

--- src/components/Category.js
+++ src/components/Category.js
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 
-export default function Categories(onSelectCategory) {
+export default function Categories({onSelectCategory}) {
   const [categories, setCategories] = useState([]);
 
    useEffect(() => {
