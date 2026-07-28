96dae7141b4abbae79e86cee71320449f6b2f44d
faisal ali zaid <zaidalideveloper@gmail.com>
2026-07-28 14:13:35 +0300

    added styling to heading

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -10,8 +10,8 @@ export default function Services() {
       </div>
 
       <div className="flex justify-between">
-        <div className="border ml-[5%] mt-[3%] pl-[2%] pr-[2%]">
-           <h2 className="border-b">Categories</h2>
+        <div className="border ml-[5%] mt-[3%]">
+           <h2 className="border-b px-4 py-3 font-semibold text-lg">Categories</h2>
           <Categories/>
         </div>
         <div className="category info">
