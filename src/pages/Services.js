990540fecf2c9336cda533432787b0ff58de6d51
faisal ali zaid <zaidalideveloper@gmail.com>
2026-07-28 13:59:24 +0300

    added some styling to categories

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -10,7 +10,7 @@ export default function Services() {
       </div>
 
       <div className="flex justify-between">
-        <div className="border">
+        <div className="border ml-[5%] mt-[3%] pl-[2%] pr-[2%]">
           <Categories/>
         </div>
         <div className="category info">
