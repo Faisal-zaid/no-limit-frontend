2fd79a8e94bd0587fe9bfe796938f122c2b8b046
faisal ali zaid <zaidalideveloper@gmail.com>
2026-07-28 15:31:31 +0300

    fetched all category code

---
+++ src/components/Categorydetails.js
@@ -0,0 +1,31 @@
+'use client';  //this tells next js this component should run on the client not only in the server side
+
+import { useEffect, useState } from "react";
+
+export default function Categories({onSelectCategory}) {
+  const [categories, setCategories] = useState([]);
+
+   useEffect(() => {
+    async function loadCategories() {
+      const response = await fetch("http://127.0.0.1:8001/category");
+      const data = await response.json();
+
+      setCategories(data);
+    }
+
+    loadCategories();
+  }, []);
+
+  return (
+    <div >
+     
+
+      {categories.map((category) => (
+        <div key={category.id}>
+          <h3 onClick={() => onSelectCategory && onSelectCategory(category)}
+           className="mt-[10%]">{category.name}</h3>
+        </div>
+      ))}
+    </div>
+  );
+}
