ca3d0acf6c51753a0c5b082a966fb1ce1ea1225c
faisal ali zaid <zaidalideveloper@gmail.com>
2026-08-04 11:51:23 +0300

    added hover effect

--- src/components/Category.js
+++ src/components/Category.js
@@ -23,7 +23,7 @@ export default function Categories({onSelectCategory}) {
       {categories.slice(0,4).map((category) => (
         <div key={category.id}>
           <h3 onClick={() => onSelectCategory && onSelectCategory(category)}
-           className="mt-[18%]">{category.name}</h3>
+           className="mt-[18%] hover:bg-purple-700 w-[70%] justify-center">{category.name}</h3>
         </div>
       ))}
     </div>
