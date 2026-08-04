2f1ba7171d931ab122aee186b7e6d06361ecb4ed
faisal ali zaid <zaidalideveloper@gmail.com>
2026-08-04 12:35:34 +0300

    added code for the new page

--- src/pages/Categoriespage.js
+++ src/pages/Categoriespage.js
@@ -1,2 +1,30 @@
 import Categories from "@/components/Category";
 import Categorydescription from "@/components/Categorydes";
+
+export default function Categoriespage({onSelectCategory}) {
+  const [categories, setCategories] = useState([]);
+
+   useEffect(() => {
+    async function loadCategories() {
+      const response = await fetch("http://127.0.0.1:8001/category");
+      const data = await response.json();
+
+      setCategories(data);
+    }
+
+    loadCategories();
+  }, []);
+
+  return (
+    <div className="flex items-center gap-15  justify-center" >
+     
+
+      {categories.slice(0,8).map((category) => (
+        <div key={category.id} >
+          <h3 onClick={() => onSelectCategory && onSelectCategory(category)}
+           className="nav">{category.name}</h3>
+        </div>
+      ))}
+    </div>
+  );
+}
