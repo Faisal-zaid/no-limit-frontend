776d82f2f7130f8add1acf1543a8f6468b0b302b
faisal ali zaid <zaidalideveloper@gmail.com>
2026-08-05 12:35:39 +0300

    styled button

--- src/pages/categoriespage.js
+++ src/pages/categoriespage.js
@@ -13,7 +13,7 @@ export default function CategoriesPage() {
     /* 1. Added bg-white (or bg-gray-50) and minimum height so the background is visible */
     <section className="min-h-screen bg-[url('/images/nolimitbackground.png')] bg-no-repeat bg-cover pt-6">
       <div className=" pt-10  items-center text-center">
-        <button className="border justify-center text-center border-purple-700 ">
+        <button className="border pt-2 pb-2 pr-3 pl-3 rounded-[30px] justify-center text-center border-purple-700 ">
           Our Sevices
         </button>
         <h2>Professional Branding Services</h2>
