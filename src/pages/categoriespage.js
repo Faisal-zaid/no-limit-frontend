ac33e7e8f54ad01ed3d756ed52d60896b6df9e0a
faisal ali zaid <zaidalideveloper@gmail.com>
2026-08-05 15:14:29 +0300

    modified the chat with us button

--- src/pages/categoriespage.js
+++ src/pages/categoriespage.js
@@ -134,14 +134,14 @@ export default function CategoriesPage() {
           </div>
           
         </div>
-        <div>
-            <h2>Ready to Transform your Brand?</h2>
+        <div className=" border border-gray-200 rounded-xl p-4 bg-white mt-15 flex flex-col items-center">
+            <h2 className="font-bold text-[18px]">Ready to Transform your Brand?</h2>
             <p>Contact us for a free Consultation</p>
             <Link
               href={whatsappUrl}
               target="_blank"
               rel="noopener noreferrer"
-              className="inline-flex items-center gap-2 px-6 py-3 bg-green-600 text-white font-semibold rounded-lg shadow-md hover:bg-green-700 transition-colors duration-200 cursor-pointer"
+              className="inline-flex items-center gap-2 mt-3 px-6 py-3 bg-green-600 text-white font-semibold rounded-lg shadow-md hover:bg-green-700 transition-colors duration-200 cursor-pointer"
             >
               {/* Optional WhatsApp SVG Icon */}
               <svg
@@ -151,7 +151,7 @@ export default function CategoriesPage() {
               >
                 <path d="M.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946.003-6.556 5.338-11.891 11.893-11.891 3.181.001 6.167 1.24 8.413 3.488 2.245 2.248 3.481 5.236 3.48 8.414-.003 6.557-5.338 11.892-11.893 11.892-1.99-.001-3.951-.5-5.688-1.448l-6.305 1.654zm6.597-3.807c1.676.995 3.276 1.591 5.392 1.592 5.448 0 9.886-4.434 9.889-9.885.002-5.462-4.415-9.89-9.881-9.892-5.452 0-9.887 4.434-9.889 9.884-.001 2.225.651 3.891 1.746 5.634l-.999 3.648 3.742-.981z" />
               </svg>
-              Consult Us
+              Chat with Us
             </Link>
           </div>
       </div>
