dfc4cef22eca13238223fe316dd1e7b31218b698
faisal ali zaid <zaidalideveloper@gmail.com>
2026-08-08 11:14:30 +0300

    added admin panel

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -1,3 +1,13 @@
 "use client";
 
 import { useState, useEffect } from "react";
+
+return(
+    <section>
+        <div>
+            <h2>
+                Admin Panel
+            </h2>
+        </div>
+    </section>
+)
