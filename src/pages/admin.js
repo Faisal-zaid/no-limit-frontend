28811a3ec520a1333709fd04477f7f88dd8b5ffe
faisal ali zaid <zaidalideveloper@gmail.com>
2026-08-08 11:23:20 +0300

    added heading for admin panel

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -4,10 +4,11 @@ import { useState, useEffect } from "react";
 export default function Admin(){
 return(
     <section className="min-h-screen bg-[url('/images/nolimitbackground.png')] bg-no-repeat bg-cover">
-        <div>
-            <h2>
+        <div className="flex gap-3">
+            <h2 >
                 Admin Panel
             </h2>
+            <button>Log out</button>
         </div>
     </section>
 )
