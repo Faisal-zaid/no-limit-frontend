b19248cdca406f2720539c532cfbbf54bfe0e220
faisal ali zaid <zaidalideveloper@gmail.com>
2026-08-08 21:57:28 +0300

    fetching categories

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -2,6 +2,18 @@
 
 import { useState, useEffect } from "react";
 export default function Admin(){
+
+    useEffect(() => {
+    async function loadCategories() {
+      const response = await fetch("http://127.0.0.1:8001/category");
+      const data = await response.json();
+
+      setCategories(data);
+    }
+
+    loadCategories();
+  }, []);
+  
 return(
     <section className="min-h-screen bg-[url('/images/nolimitbackground.png')] bg-no-repeat bg-cover">
         <div className="flex gap-3">
