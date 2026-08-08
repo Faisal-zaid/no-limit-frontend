a3dd9a6a4d54eda1db4d767fe786090640adc755
faisal ali zaid <zaidalideveloper@gmail.com>
2026-08-08 21:59:40 +0300

    fetching products

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -16,6 +16,18 @@ export default function Admin(){
     loadCategories();
   }, []);
 
+  //fetching products
+  useEffect(() => {
+    async function loadProducts() {
+      const response = await fetch("http://127.0.0.1:8001/category");
+      const data = await response.json();
+
+      setProducts(data);
+    }
+
+    loadProducts();
+  }, []);
+
 return(
     <section className="min-h-screen bg-[url('/images/nolimitbackground.png')] bg-no-repeat bg-cover">
         <div className="flex gap-3">
