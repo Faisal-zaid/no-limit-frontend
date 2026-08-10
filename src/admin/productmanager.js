fbe9482ccdcb7df43dbb47860d31c97e9338f764
faisal ali zaid <zaidalideveloper@gmail.com>
2026-08-10 10:54:05 +0300

    finished products handling

--- src/admin/productmanager.js
+++ src/admin/productmanager.js
@@ -1,53 +1,56 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { getCategories, deleteCategory } from "@/lib/api";
+
 export default function ProductManager() {
   const [products, setProducts] = useState([]);
-  
-  // fetching categories
+
+  // GET products
   useEffect(() => {
     async function loadProducts() {
-      const response = await fetch("http://127.0.0.1:8001/product");
+      const response = await fetch(
+        "http://127.0.0.1:8001/product"
+      );
+
       const data = await response.json();
 
       setProducts(data);
     }
 
-    loadCategories();
+    loadProducts();
   }, []);
 
-  // creating category
-
+  // POST product
   async function createProduct(productData) {
     const formData = new FormData();
 
     formData.append("name", productData.name);
-    formData.append("name", productData.category_id);
+    formData.append("category_id", productData.category_id);
+    formData.append("base_price", productData.base_price);
     formData.append("description", productData.description);
-    formData.append("subheading", productData.base_price);
     formData.append("image", productData.image);
 
-    const response = await fetch("http://127.0.0.1:8001/product", {
-      method: "POST",
-      body: formData,
-    });
+    const response = await fetch(
+      "http://127.0.0.1:8001/product",
+      {
+        method: "POST",
+        body: formData,
+      }
+    );
 
     const data = await response.json();
 
     console.log(data);
   }
 
-  //patch category
-
+  // PATCH product
   async function updateProduct(productId, productData) {
     const formData = new FormData();
 
     formData.append("name", productData.name);
-    formData.append("name", productData.category_id);
+    formData.append("category_id", productData.category_id);
+    formData.append("base_price", productData.base_price);
     formData.append("description", productData.description);
-    formData.append("subheading", productData.base_price);
-    formData.append("image", productData.image);
 
     if (productData.image) {
       formData.append("image", productData.image);
@@ -58,7 +61,7 @@ export default function ProductManager() {
       {
         method: "PATCH",
         body: formData,
-      },
+      }
     );
 
     const data = await response.json();
@@ -66,14 +69,13 @@ export default function ProductManager() {
     console.log(data);
   }
 
-  //delete category
-
+  // DELETE product
   async function deleteProduct(productId) {
     const response = await fetch(
-      `http://127.0.0.1:8001/category/${productId}`,
+      `http://127.0.0.1:8001/product/${productId}`,
       {
         method: "DELETE",
-      },
+      }
     );
 
     const data = await response.json();
@@ -81,13 +83,20 @@ export default function ProductManager() {
     console.log(data);
   }
 
-  
   return (
-    <section className="min-h-screen bg-[url('/images/nolimitbackground.png')] bg-no-repeat bg-cover">
-      <div className="flex gap-3">
-        <h2>Admin Panel</h2>
-        <button>Log out</button>
-      </div>
-    </section>
+    <div>
+      <h1>Admin Panel</h1>
+
+      {products.map((product) => (
+        <div key={product.id}>
+          <h2>{product.name}</h2>
+          <p>{product.base_price}</p>
+
+          <button onClick={() => deleteProduct(product.id)}>
+            Delete
+          </button>
+        </div>
+      ))}
+    </div>
   );
 }
