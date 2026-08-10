c8a36031dd49b8d8208b990884896bad82f2d7c6
faisal ali zaid <zaidalideveloper@gmail.com>
2026-08-10 22:36:30 +0300

    added icon to logout

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -15,7 +15,11 @@ export default function Admin() {
       <div className="flex justify-between ml-7 mr-7">
         <div></div>
         <div><h2 className="text-[30px]">Admin Panel</h2></div>
-        <div><button>Log out</button></div>
+        <div><button className="flex items-center"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+	<path d="M0 0h24v24H0z" fill="none" />
+	<path fill="currentColor" d="M5 21q-.825 0-1.412-.587T3 19V5q0-.825.588-1.412T5 3h7v2H5v14h7v2zm11-4l-1.375-1.45l2.55-2.55H9v-2h8.175l-2.55-2.55L16 7l5 5z" />
+</svg>
+Log out</button></div>
       </div>
       <div className="space-y-10">
 
