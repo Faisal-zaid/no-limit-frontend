44fc4523957bd7e0330b6b734bd554dd59778bf6
faisal ali zaid <zaidalideveloper@gmail.com>
2026-08-10 11:28:35 +0300

    imported product field manager

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 import { getCategories, deleteCategory } from "@/lib/api";
 import CategoryManager from "@/admin/categorymanager";
 import ProductManager from "@/admin/productmanager";
+import ProductFieldManager from "@/admin/productfieldmanager";
 export default function Admin() {
   const [categories, setCategories] = useState([]);
   const [products, setProducts] = useState([]);
