84f047aab1392a2913be208129c0262c18bfd57a
faisal ali zaid <zaidalideveloper@gmail.com>
2026-08-14 10:23:56 +0300

    added a font file

---
+++ src/app/font.js
@@ -0,0 +1,11 @@
+import { Rancho, Londrina_Solid } from "next/font/google";
+
+export const rancho = Rancho({
+  subsets: ["latin"],
+  weight: "400",
+});
+
+export const londrina = Londrina_Solid({
+  subsets: ["latin"],
+  weight: "400",
+});
