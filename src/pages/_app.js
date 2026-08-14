3944b166d8ed7edae527a19d5e711ae5476de054
faisal ali zaid <zaidalideveloper@gmail.com>
2026-08-14 09:44:50 +0300

    modified _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,5 +2,8 @@
 import "@/app/globals.css"; // Make sure this path points to your globals.css!
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return 
+   <CartProvider>
+      <Component {...pageProps} />
+    </CartProvider>
 }
