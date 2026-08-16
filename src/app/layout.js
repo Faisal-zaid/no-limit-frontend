db947c3e75394680e4587fa185ea4996fe051c78
faisal ali zaid <zaidalideveloper@gmail.com>
2026-08-16 13:09:28 +0300

    commented out imports of font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers";
-import { rancho, londrina } from "./fonts";
+// import { rancho, londrina } from "./fonts";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
