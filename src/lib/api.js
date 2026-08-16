08f2c07b4dda24b02df545d6b9ddfe8ad22d37be
faisal ali zaid <zaidalideveloper@gmail.com>
2026-08-16 11:27:42 +0300

    added .env link

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8001";
+const API_URL = "${process.env.NEXT_PUBLIC_API_URL}";
 
 export async function getCategories() {
   const response = await fetch(`${API_URL}/category`);
