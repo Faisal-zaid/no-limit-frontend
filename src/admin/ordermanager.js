dff239d0b30a9ec74997471ffe9ef8599f041621
faisal ali zaid <zaidalideveloper@gmail.com>
2026-08-17 11:17:36 +0300

    corrrected the url

--- src/admin/ordermanager.js
+++ src/admin/ordermanager.js
@@ -16,7 +16,7 @@ export default function OrderManager() {
     try {
 
       const response = await fetch(
-        "http://localhost:8000/order"
+        `${process.env.NEXT_PUBLIC_API_URL}/order`
       );
 
       if (!response.ok) {
